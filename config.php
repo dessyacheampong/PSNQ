8d12920a4177e6ec1df513f64973befefed23c45
dessyacheampong <77683806+dessyacheampong@users.noreply.github.com>
2025-01-24 12:39:51 +0000

    Create config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$conn = new mysqli("localhost", "your_username", "your_password", "your_database");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
+<?php
+$conn = new mysqli("localhost", "your_username", "your_password", "your_database");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
